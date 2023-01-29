6b279a880c234c3b81b3432f86bffe343b152eec
Estarlin R <estarlincito@estarlincitoM.local>
2023-01-29 01:05:02 -0400

    1.1.7

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,36 @@
 import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Head from "next/head";
-import style from "../styles/home.module.css";
+// import style from "../styles/home.module.css";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Home = () => {
   const router = useRouter();
+  const { timeboxing } = useContext(UserContext);
+
   const handleClick = () => {
     router.push("/new");
   };
 
-  return (
-    <>
-      <Head>
-        <title>Timeboxing</title>
-      </Head>
+  if (timeboxing.length === 0) {
+    return (
+      <>
+        <Head>
+          <title>Timeboxing</title>
+        </Head>
 
-      <Header />
-      <main className={style.main}>
-        <div className={style.btn} onClick={handleClick}>
-          <h2 className="title">Empezar</h2>
-        </div>
-      </main>
-    </>
-  );
+        <Header />
+        <main className={"style.main"}>
+          <div className={"style.btn"} onClick={handleClick}>
+            <h2 className="title">Empezar</h2>
+          </div>
+        </main>
+      </>
+    );
+  } else {
+    router.replace("/timeboxing");
+  }
 };
 
 export default Home;
